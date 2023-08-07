a834c85c9145217b30dff116985cae96454149d2
patiee <zawpatrycja@gmail.com>
2023-08-07 19:12:49 +0200

    Add Firehose extractor

--- node/node.go
+++ node/node.go
@@ -20,6 +20,7 @@ import (
 	cfg "github.com/cometbft/cometbft/config"
 	cs "github.com/cometbft/cometbft/consensus"
 	"github.com/cometbft/cometbft/crypto"
+	"github.com/cometbft/cometbft/deepmind"
 	"github.com/cometbft/cometbft/evidence"
 
 	cmtjson "github.com/cometbft/cometbft/libs/json"
@@ -749,6 +750,12 @@ func NewNode(config *cfg.Config,
 		return nil, err
 	}
 
+	// Initialize data extraction
+	if config.Extractor.Enabled {
+		deepmind.Initialize(config.Extractor)
+		logger.Info("Initialized extractor module", "output", config.Extractor.OutputFile)
+	}
+
 	// If an address is provided, listen on the socket for a connection from an
 	// external signing process.
 	if config.PrivValidatorListenAddr != "" {
@@ -1056,6 +1063,10 @@ func (n *Node) OnStop() {
 		if err := n.stateStore.Close(); err != nil {
 			n.Logger.Error("problem closing statestore", "err", err)
 		}
+	}
+	if deepmind.IsEnabled() {
+		n.Logger.Info("waiting for last block finalization", "module", "deepmind")
+		deepmind.Shutdown(context.Background())
 	}
 }
 
